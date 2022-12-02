5d017bd7635e3e1c8bb1f4756b43045c4ae0e24f
Brunno Gorgulho Soares <37427570+BGSoares@users.noreply.github.com>
2022-12-02 22:54:44 +0000

    Refactored day 1 code.

--- day-1/main.py
+++ day-1/main.py
@@ -50,18 +50,42 @@
 
 # Your puzzle answer was 71124.
 
-f = open("day-1/input.txt", mode='r')
+# --- Part Two ---
+# By the time you calculate the answer to the Elves' question, 
+# they've already realized that the Elf carrying the most Calories 
+# of food might eventually run out of snacks.
 
-max_elf = 0
-cur_elf = 0
+# To avoid this unacceptable situation, the Elves would instead 
+# like to know the total Calories carried by the top three Elves 
+# carrying the most Calories. That way, even if one of those Elves 
+# runs out of snacks, they still have two backups.
 
-for line in f:
-    if line == "\n":
-        if cur_elf > max_elf:
-            max_elf = cur_elf
-        cur_elf = 0
-    else:
-        line = int(line)
-        cur_elf = cur_elf + line
+# In the example above, the top three Elves are the fourth Elf 
+# (with 24000 Calories), then the third Elf (with 11000 Calories), 
+# then the fifth Elf (with 10000 Calories). The sum of the Calories 
+# carried by these three elves is 45000.
 
-print("max_elf: ", max_elf)
+# Find the top three Elves carrying the most Calories. How many 
+# Calories are those Elves carrying in total?
+
+# Your puzzle answer was 204639.
+
+def reformat(data):
+    data = data.split("\n\n")
+    data = data[:-1]
+    data = [map(int, d.split("\n")) for d in data]
+    data = [sum(elf) for elf in data]
+    return data
+
+def solve_p1(data):
+    return max(data)
+
+def solve_p2(data):
+    return sum(sorted(data)[-3:])
+
+if __name__ == "__main__":
+    with open("day-1/input.txt") as input:
+        data = input.read()
+    data = reformat(data)
+    print(f"Solution to problem 1 is: {solve_p1(data)}")
+    print(f"Solution to problem 2 is: {solve_p2(data)}")
